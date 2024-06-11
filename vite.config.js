3ee691ea08a0293c9d72ef0bba51ae2a9a6a10ae
riyan <riyansk255@gmail.com>
2024-06-11 23:42:19 +0530

    updated vite.conf file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), ghPages()],
+  base: Todo-List-ReactBasics, // Replace 'your-repository-name' with your repo name
+});
